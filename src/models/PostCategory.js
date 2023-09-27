4a4d7a5ca86da3fe7a71a5b217f15e3319d97b60
albertoflorence <alberto.sabatier@gmail.com>
2023-09-27 19:54:53 -0300

    requisito 11 - create associations between BlogPost and Category

---
+++ src/models/PostCategory.js
@@ -0,0 +1,41 @@
+/**
+ * @param {import('sequelize').Sequelize} sequelize
+ * @param {import('sequelize').DataTypes} DataTypes
+ */
+
+const model = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define(
+    'PostCategory',
+    {
+      categoryId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+      },
+      postId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+      },
+    },
+    { timestamps: false, tableName: 'post_categories', underscored: true },
+  );
+
+  PostCategory.associate = ({ BlogPost, Category }) => {
+    BlogPost.belongsToMany(Category, {
+      foreignKey: 'postId',
+      as: 'category',
+      through: PostCategory,
+    });
+
+    Category.belongsToMany(BlogPost, {
+      foreignKey: 'categoryId',
+      as: 'blogPosts',
+      through: PostCategory,
+    });
+  };
+
+  return PostCategory;
+};
+
+module.exports = model;

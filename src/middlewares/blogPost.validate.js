c0a3d3401db0beccbe0efd2c987f11eabf849f0d
albertoflorence <alberto.sabatier@gmail.com>
2023-09-28 00:41:53 -0300

    fix typo on blogPost.validate

--- src/middlewares/blogPost.validate.js
+++ src/middlewares/blogPost.validate.js
@@ -3,14 +3,14 @@ const handleValidate = require('../utils/handleValidate');
 
 const title = joi.string().required();
 const content = joi.string().required();
-const categoryId = joi.array().items(joi.number()).required();
+const categoryIds = joi.array().items(joi.number()).required();
 
 const messages = {
   'any.required': 'Some required fields are missing',
   'string.empty': 'Some required fields are missing',
 };
 
-const create = handleValidate(joi.object({ title, content, categoryId }).messages(messages));
+const create = handleValidate(joi.object({ title, content, categoryIds }).messages(messages));
 const update = handleValidate(joi.object({ title, content }).messages(messages));
 
 module.exports = { create, update };
